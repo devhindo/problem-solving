8d201f5de05c041c822ac9f5bcbe4cd146c681ec
hindawi <ahmedhindawi2001@gmail.com>
2022-08-04 14:38:21 +0200

    recursive solution

--- ICPCAssuit/7. Recursion/C. Print from N to 1.cpp
+++ ICPCAssuit/7. Recursion/C. Print from N to 1.cpp
@@ -2,14 +2,19 @@
 
 using namespace std;
 
+void PrintReverseOrder(int N) {
+    if(N<=1) {
+        cout << N;
+        return;
+    } else {
+        cout << N << " ";
+        PrintReverseOrder(N -1);
+    }
+
+}
+
 int main() {
     int n;
     cin >> n;
-    for(int i=n; i >= 1; i--) {
-        if(i==1) {
-            cout << i;
-            break;
-        }
-        cout << i << " ";
-    }
+    PrintReverseOrder(n);
 }

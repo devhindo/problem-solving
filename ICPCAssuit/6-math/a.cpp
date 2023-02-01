65e3b3b8c2bce6371f6f8624dafe62605509b882
hindawi <ahmedhindawi2001@gmail.com>
2023-02-01 20:46:53 +0200

    bm

---
+++ ICPCAssuit/6-math/a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    long long n;
+    cin >> n;
+    if(n-1 & n) {
+        cout << "NO" << endl;
+    } else {
+        cout << "YES" << endl;
+    }
+}
